b9db48a13c1630e8de3f814dbae9d08fda98e708
leechangjoon <2018110021@dongguk.edu>
2024-01-25 20:04:53 +0900

    [Design] Card Component 레이아웃

---
+++ src/components/mainpage/Card/Card.tsx
@@ -0,0 +1,8 @@
+import React from "react";
+import * as S from "./style";
+
+const Card = () => {
+  return <S.CardBox>Card.tsx</S.CardBox>;
+};
+
+export default Card;
---
+++ src/components/mainpage/Card/style.ts
@@ -0,0 +1,14 @@
+import styled from "styled-components";
+
+export const CardBox = styled.div`
+  display: flex;
+  width: 450px;
+  height: 131px;
+  padding: 16px;
+  flex-direction: column;
+  justify-content: center;
+  align-items: flex-start;
+  gap: 10px;
+  flex-shrink: 0;
+  background-color: blue;
+`;

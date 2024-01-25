2b9168f3532a4b36e04ae1ff94ecd0c6f7400a49
leechangjoon <2018110021@dongguk.edu>
2024-01-25 20:06:20 +0900

    [Design] 최신순/오래된순 텍스트  레이아웃

---
+++ src/components/mainpage/SortOption/SortOption.tsx
@@ -0,0 +1,15 @@
+import React from "react";
+import * as S from "./style";
+
+function SortOption() {
+  return (
+    <S.OptionContainer>
+      <p>
+        <span>최신순</span>
+        <span>오래된 순</span>
+      </p>
+    </S.OptionContainer>
+  );
+}
+
+export default SortOption;
---
+++ src/components/mainpage/SortOption/style.ts
@@ -0,0 +1,7 @@
+import styled from "styled-components";
+
+export const OptionContainer = styled.div`
+  width: 185px;
+  height: 14px;
+  background-color: purple;
+`;

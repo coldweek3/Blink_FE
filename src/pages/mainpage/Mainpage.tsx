8e913f529920a4128701157f5a2e5fd5784831e7
leechangjoon <2018110021@dongguk.edu>
2024-01-27 23:13:50 +0900

    [Comment] 구글맵이 들어갈 위치에 대한 주석을 추가한다.

--- src/pages/mainpage/Mainpage.tsx
+++ src/pages/mainpage/Mainpage.tsx
@@ -3,6 +3,7 @@ import CardContainer from "../../components/mainpage/CardContainer/CardContainer
 
 const Mainpage = () => {
   return <CardContainer />;
+  // 여기엔 구글맵 컴포넌트가 들어갈 예정
 };
 
 export default Mainpage;

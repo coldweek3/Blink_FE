5f8efec762947018867fe515a017265ce6cb70dd
leechangjoon <2018110021@dongguk.edu>
2024-01-27 22:51:08 +0900

    [Feat] 기본 설정 버튼을 list로 두고, 버튼 클릭시 상태를 변경한다.

--- src/components/mainpage/ListButton/ListButton.tsx
+++ src/components/mainpage/ListButton/ListButton.tsx
@@ -1,11 +1,27 @@
-import React from "react";
+import React, { useState } from "react";
 import * as S from "./style";
 
 const ListButton = () => {
+  const [selectedButton, setSelectedButton] = useState("list");
+
+  const handleButtonClick = (buttonName: string) => {
+    setSelectedButton(buttonName);
+  };
+
   return (
     <S.BoxContainer>
-      <button>전체 목록</button>
-      <button>글 등록하기</button>
+      <S.ListButton
+        selected={selectedButton === "list"}
+        onClick={() => handleButtonClick("list")}
+      >
+        전체 목록
+      </S.ListButton>
+      <S.PostButton
+        selected={selectedButton === "post"}
+        onClick={() => handleButtonClick("post")}
+      >
+        글 등록하기
+      </S.PostButton>
     </S.BoxContainer>
   );
 };

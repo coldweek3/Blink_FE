b6e15c6d074745419f29db4678378a52ce1ad29c
leechangjoon <2018110021@dongguk.edu>
2024-01-27 23:15:26 +0900

    [Design] 선택 옵션에 따라 디자인을 변경한다.

--- src/components/mainpage/SortOption/style.ts
+++ src/components/mainpage/SortOption/style.ts
@@ -1,7 +1,36 @@
-import styled from "styled-components";
+import styled, { css } from "styled-components";
+
+interface OptionProps {
+  selected: boolean;
+}
 
 export const OptionContainer = styled.div`
   width: 185px;
-  height: 14px;
-  background-color: purple;
+  background-color: #ffffff;
+  display: flex;
+  flex-direction: row;
+  justify-content: space-around;
+  margin: 10px 0 10px auto;
+`;
+
+const optionStyle = css`
+  color: #000;
+  text-align: center;
+  font-family: Inter;
+  font-size: 14px;
+  font-style: normal;
+  font-weight: 600;
+  line-height: normal;
+`;
+
+export const RecentOption = styled.span<OptionProps>`
+  ${optionStyle}
+  font-weight: bold;
+  color: ${(props) => (props.selected ? "#34446D" : "#BABABA")};
+`;
+
+export const OldOption = styled.span<OptionProps>`
+  ${optionStyle}
+  font-weight: bold;
+  color: ${(props) => (props.selected ? "#34446D" : "#BABABA")};
 `;

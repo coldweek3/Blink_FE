4a0198c6d32cf595d7d99d080337305fa134b859
leechangjoon <2018110021@dongguk.edu>
2024-01-27 23:15:05 +0900

    [Feat] 기본 선택 옵션을 설정하고, 텍스트를 클릭하면 선택 옵션을 변경한다.

--- src/components/mainpage/SortOption/SortOption.tsx
+++ src/components/mainpage/SortOption/SortOption.tsx
@@ -1,13 +1,26 @@
-import React from "react";
+import React, { useState } from "react";
 import * as S from "./style";
 
 function SortOption() {
+  const [selectedOption, setSelectedOption] = useState("recent");
+
+  const handleButtonClick = (option: string) => {
+    setSelectedOption(option);
+  };
   return (
     <S.OptionContainer>
-      <p>
-        <span>최신순</span>
-        <span>오래된 순</span>
-      </p>
+      <S.RecentOption
+        selected={selectedOption === "recent"}
+        onClick={() => handleButtonClick("recent")}
+      >
+        · 최신순
+      </S.RecentOption>
+      <S.OldOption
+        selected={selectedOption === "old"}
+        onClick={() => handleButtonClick("old")}
+      >
+        · 오래된 순
+      </S.OldOption>
     </S.OptionContainer>
   );
 }

95623be73a54dcfa0e8c46dccbde41071a292939
leechangjoon <2018110021@dongguk.edu>
2024-01-28 04:17:24 +0900

    [Design] 검색 기록 태그 레이아웃을 추가한다

--- src/components/mainpage/Search/Search.tsx
+++ src/components/mainpage/Search/Search.tsx
@@ -2,9 +2,20 @@ import React from "react";
 import * as S from "./style";
 
 function Search() {
+  // const displayText = text.length > 6 ? `${text.slice(0, 6)}...` : text;
   return (
     <S.Container>
       <S.Input />
+      <span>최근 검색 지역</span>
+      <S.TagConatainer>
+        <S.Tag>하</S.Tag>
+        <S.Tag>하나</S.Tag>
+        <S.Tag>하나나나</S.Tag>
+        <S.Tag>하나나나나</S.Tag>
+        <S.Tag>하나나나나나</S.Tag>
+        <S.Tag>하나나나나나</S.Tag>
+        <S.Tag>하나나나나나</S.Tag>
+      </S.TagConatainer>
     </S.Container>
   );
 }
--- src/components/mainpage/Search/style.ts
+++ src/components/mainpage/Search/style.ts
@@ -3,7 +3,8 @@ import search from "../../../assets/Icon/search.svg";
 
 export const Container = styled.div`
   display: flex;
-  align-items: center;
+  flex-direction: column;
+  /* align-items: center; */
   justify-content: center;
   width: 490px;
   height: 214px;
@@ -38,5 +39,49 @@ export const Input = styled.input.attrs({
   // 입력창 포커스 시 스타일
   &:focus {
     border-color: #007aff; // 포커스 됐을 때 테두리 색상
+  }
+`;
+
+export const TagConatainer = styled.div`
+  padding: 8px 0px;
+  gap: 10px;
+  background-color: red;
+  overflow-x: scroll;
+  white-space: nowrap;
+
+  ::-webkit-scrollbar {
+    display: none;
+  }
+`;
+
+export const Tag = styled.div`
+  display: inline-flex;
+  align-items: center;
+  width: auto;
+  padding: 8px 12px;
+  margin: 4px;
+  font-size: 14px;
+  border-radius: 20px;
+  border: none;
+  background: #f6f9ff;
+  box-shadow: 0px 2px 4px rgba(0, 0, 0, 0.1);
+  white-space: nowrap;
+  overflow: hidden;
+  text-overflow: ellipsis; // 넘치는 텍스트를 말줄임표로 표시
+
+  // 태그 내 'X' 버튼에 대한 스타일
+  .close-icon {
+    display: inline-flex; // 아이콘을 inline-flex로 설정
+    justify-content: center;
+    align-items: center;
+    margin-left: 8px; // 아이콘과 텍스트 사이의 간격을 추가
+    cursor: pointer; // 마우스 오버 시 커서 변경
+  }
+
+  // 태그 텍스트의 최대 너비를 지정하여 글자 수에 따라 말줄임표를 적용
+  .text {
+    max-width: 80px; // 최대 너비를 설정하여 말줄임표를 적용할 길이를 결정
+    overflow: hidden;
+    text-overflow: ellipsis;
   }
 `;

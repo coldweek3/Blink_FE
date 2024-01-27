b76aece3f4072eb3ddf9399ec84c6e8560b3753b
leechangjoon <2018110021@dongguk.edu>
2024-01-27 22:52:04 +0900

    [Design] 버튼의 클릭 여부에 따라 다른 색으로 렌더링한다.

--- src/components/mainpage/ListButton/style.ts
+++ src/components/mainpage/ListButton/style.ts
@@ -1,6 +1,32 @@
-import styled from "styled-components";
+import styled, { css } from "styled-components";
+
+interface ButtonProps {
+  selected: boolean;
+}
 
 export const BoxContainer = styled.div`
   width: 448px;
   height: 55px;
+  display: flex;
+  flex-direction: row;
+`;
+
+const buttonStyle = css`
+  width: 224.508px;
+  height: 55px;
+  flex-shrink: 0;
+  border-radius: 10px;
+  color: white;
+`;
+
+export const ListButton = styled.button<ButtonProps>`
+  ${buttonStyle}
+  border-radius: 10px 0px 0px 10px;
+  background-color: ${(props) => (props.selected ? "#34446D" : "#BABABA")};
+`;
+
+export const PostButton = styled.button<ButtonProps>`
+  ${buttonStyle}
+  border-radius: 0px 10px 10px 0px;
+  background-color: ${(props) => (props.selected ? "#34446D" : "#BABABA")};
 `;

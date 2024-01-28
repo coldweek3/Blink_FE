754cb2040abfad92e2208d7c5f18b1cfa9eb5cdd
leechangjoon <2018110021@dongguk.edu>
2024-01-28 16:56:34 +0900

    [Feat] 사용자 입력에 따라 태그를 생성하고, X버튼을 누르면 태그를 삭제한다.

--- src/components/mainpage/Search/Search.tsx
+++ src/components/mainpage/Search/Search.tsx
@@ -1,15 +1,40 @@
-import React from "react";
+import React, { useState } from "react";
 import * as S from "./style";
 import Tag from "./Tag";
 
 function Search() {
+  // 태그 길이 길어지면 ... 으로 자를까 해서 미리 작성해둠.
   // const displayText = text.length > 6 ? `${text.slice(0, 6)}...` : text;
+  const [inputText, setInputText] = useState<string>("");
+  const [tags, setTags] = useState<string[]>([]);
+
+  const handleInputChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+    setInputText(e.target.value);
+  };
+
+  const handleKeyDown = (e: React.KeyboardEvent) => {
+    if (e.key === "Enter" && inputText) {
+      setTags([...tags, inputText]);
+      setInputText("");
+    }
+  };
+
+  const deleteTag = (index: number) => {
+    setTags(tags.filter((_, idx) => idx !== index));
+  };
+
   return (
     <S.Container>
-      <S.Input />
+      <S.Input
+        value={inputText}
+        onChange={handleInputChange}
+        onKeyDown={handleKeyDown}
+      />
       <span>최근 검색 지역</span>
       <S.TagConatainer>
-        <Tag />
+        {tags.map((tag, index) => (
+          <Tag key={index} text={tag} onDelete={() => deleteTag(index)} />
+        ))}
       </S.TagConatainer>
     </S.Container>
   );
--- src/components/mainpage/Search/Tag.tsx
+++ src/components/mainpage/Search/Tag.tsx
@@ -1,30 +1,18 @@
 import React from "react";
 import * as S from "./style";
 
-function Tag() {
-  const recentSearchData = [
-    "동국대학교",
-    "충무로",
-    "종로",
-    "종각",
-    "혜화",
-    "압구정",
-  ];
-
-  function buttonClick() {
-    alert("X 버튼 클릭 이벤트 발생");
-  }
-
-  return (
-    <div>
-      {recentSearchData.map((data, index) => (
-        <S.Tag key={index}>
-          {data}
-          <S.XButton onClick={buttonClick} />
-        </S.Tag>
-      ))}
-    </div>
-  );
+interface TagProps {
+  text: string;
+  onDelete: () => void;
 }
+
+const Tag: React.FC<TagProps> = ({ text, onDelete }) => {
+  return (
+    <S.Tag>
+      {text}
+      <S.XButton onClick={onDelete} />
+    </S.Tag>
+  );
+};
 
 export default Tag;

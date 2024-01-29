d4e2cc330fb2e428a70d565c3ef5b786e3564d5c
leechangjoon <2018110021@dongguk.edu>
2024-01-30 02:08:55 +0900

    [Test] 버튼 클릭 이벤트 작동 유무를 확인한다.

--- src/components/mainpage/Card/CardInnerImage.tsx
+++ src/components/mainpage/Card/CardInnerImage.tsx
@@ -3,12 +3,19 @@ import * as S from "./style";
 import testMap from "../../../assets/image/test/testMap.svg";
 
 function CardInnerImage() {
+  function Event() {
+    alert("버튼 클릭 이벤트 발생");
+  }
   return (
     <S.ImageContainer>
       <S.InnerImage src={testMap} />
-      <S.ButtonTopLeft>블박 플레이</S.ButtonTopLeft>
-      <S.ButtonBottomRightFirst>찾아요</S.ButtonBottomRightFirst>
-      <S.ButtonBottomRightSecond>제보해요</S.ButtonBottomRightSecond>
+      <S.ButtonTopLeft onClick={Event}>블박 플레이</S.ButtonTopLeft>
+      <S.ButtonBottomRightFirst onClick={Event}>
+        찾아요
+      </S.ButtonBottomRightFirst>
+      <S.ButtonBottomRightSecond onClick={Event}>
+        제보해요
+      </S.ButtonBottomRightSecond>
     </S.ImageContainer>
   );
 }

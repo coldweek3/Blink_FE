5c66461ceaf7079759546a5c6e1d0bbf31a72ec3
leechangjoon <2018110021@dongguk.edu>
2024-01-30 02:11:01 +0900

    [Remove] 미사용 코드를 제거한다.

--- src/components/mainpage/Card/Card.tsx
+++ src/components/mainpage/Card/Card.tsx
@@ -3,19 +3,9 @@ import * as S from "./style";
 import CardInnerImage from "./CardInnerImage";
 
 const Card = () => {
-  function handleButtonClick() {
-    alert("버튼 클릭 이벤트 발생");
-  }
-
   return (
     <S.CardBox>
       <CardInnerImage />
-      {/* <S.ButtonContainer>
-        <S.ButtonPrimary onClick={handleButtonClick}>진행 중</S.ButtonPrimary>
-        <S.ButtonSecondary onClick={handleButtonClick}>
-          찾아요
-        </S.ButtonSecondary>
-      </S.ButtonContainer> */}
       <S.ContentTitle>인생이 힘들다</S.ContentTitle>
       <S.ContentInfoContainer>
         <S.ContentInfoText>2024.01.30 | 조회 2,000</S.ContentInfoText>
